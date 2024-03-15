4ce577eca98cd54cb2ed957a864fb44af0cfb3f9
nisrinasals <nisrinahikmawan@gmail.com>
2024-03-15 14:42:50 +0700

    menambahkan display awal dan input

--- 054_InsertionSort/054_InsertionSort.cpp
+++ 054_InsertionSort/054_InsertionSort.cpp
@@ -4,6 +4,28 @@ using namespace std;
 int arr[12];
 int n;
 
+void input() { //prosedur untuk input
+	while (true) {
+		cout << "Masukkan banyaknya elemen pada array : ";	//output ke layar
+		cin >> n;	//input dari pengguna
+		if (n <= 20)	//jika n kurang dari atau sama dengan 20
+			break;	//keluar dari loop
+		else {	//jika n lebih dari 20
+			cout << "\nArray dapat mempunyai maksimal 20 elemen. \n";	//output ke layar
+		}
+	}
+	cout << endl;
+	cout << "=====================" << endl;
+	cout << "Masukkan Elemen Array" << endl;
+	cout << "=====================" << endl;
+
+	for (int i = 0; i < n; i++) {	//looping dengan i dimulai dari 0 hingga n-1
+		cout << "Data ke-" << (i + 1) << ": ";	//output ke layar
+		cin >> arr[i];	//output dari pengguna
+	}
+}
+
+
 void insertionsort() {
 
 	int i, temp, j;
@@ -17,11 +39,16 @@ void insertionsort() {
 			j = j - 1;	//step 4b
 		}
 		arr[j + 1] = temp;	//step 5
+
 	}
 }
 
 int main()
 {
+	input();
+	insertionsort();
+	display();
+
 	system("pause");
 	return 0;
 }
